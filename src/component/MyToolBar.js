c9e2ebf940515a43e8b34c6d5513612b607e91f6
SimplyY <SimplyYu@163.com>
2016-05-02 15:36:10 +0800

    set StatusBar in MyToolBar.js

--- src/component/MyToolBar.js
+++ src/component/MyToolBar.js
@@ -2,7 +2,8 @@ import React, {
     Component,
     StyleSheet,
     View,
-    ToolbarAndroid
+    ToolbarAndroid,
+    StatusBar
 } from 'react-native'
 
 import { COLOR } from '../common/const'
@@ -11,6 +12,10 @@ class MyToolBar extends Component {
     render() {
         return (
             <View style={styles.pageContainer}>
+                <StatusBar
+                    backgroundColor={COLOR.litteBlack}
+                    barStyle="light-content"
+                    />
                 <ToolbarAndroid
                     style={styles.toolbar}
                     title={this.props.title}
